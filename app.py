d2cd608122c0a10742553525f24f593f182f5456
khatrabiru <khatrabiru@gmail.com>
2022-12-26 16:34:37 +0000

    Initial commit which generates the porofile

--- app.py
+++ app.py
@@ -1,35 +1,70 @@
 import os
-
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+questions = {
+    "Actor": [
+        "Biography",
+        "Childhood",
+        "Education",
+        "Personal Life",
+        "Family",
+        "Films",
+        "Awards",
+        "Social Media links"
+    ],
+    "Politician": [
+        "Biography",
+        "Childhood",
+        "Education",
+        "Personal Life",
+        "Family",
+        "Political Party Details",
+        "Awards or Notable Works",
+        "Social Media links"
+    ],
+    "Singer": [
+        "Biography",
+        "Childhood",
+        "Education",
+        "Personal Life",
+        "Family",
+        "Songs",
+        "Awards",
+        "Social Media links"
+    ],
+    "Player": [
+        "Biography",
+        "Childhood",
+        "Education",
+        "Personal Life",
+        "Family",
+        "Sports Career",
+        "Awards or Notable Works",
+        "Social Media links"
+    ]
+}
+
 
 @app.route("/", methods=("GET", "POST"))
 def index():
+    results = []
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+        name = request.form["name"]
+        profession = request.form["profession"]
+        for question in questions[profession]:
+            heading = profession + ' ' + name + ' ' + question
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=heading,
+                temperature=0.7,
+                max_tokens=256,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+            )
+            results.append((question, response.choices[0].text))
+    return render_template("index.html", results=results)
